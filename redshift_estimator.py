0ae4300d63be02da349c78900389b904d57dc80e
Jacob Bieker <jacob@bieker.tech>
2019-08-29 11:23:59 +0200

    Basic redshift estimator started
    Results are not great so far, but now the data loader and such are setup to work

--- redshift_estimator.py
+++ redshift_estimator.py
@@ -1,6 +1,60 @@
 import h5py
-import numpy
+import numpy as np
+from redshift_loader import DataLoader
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D, MaxPooling2D, Flatten, Dense
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+import keras
 
-f = h5py.File("network/0-4000.hdf5", 'r')
+from keras.optimizers import Adam
 
-print(f.keys())
+# Input shape
+img_rows = 64
+img_cols = 64
+channels = 5
+img_shape = (img_rows, img_cols, channels)
+
+# Configure data loader
+data = np.loadtxt("network/networkFrame.csv", delimiter=',', dtype=str)
+
+train = DataLoader(img_res=(img_rows, img_cols), batch_size=32, norm=False, data=data[:int(len(data)/2)])
+val = DataLoader(img_res=(img_rows, img_cols), batch_size=32, norm=False, data=data[int(len(data)/2):])
+
+model = Sequential()
+model.add(Conv2D(32, input_shape=img_shape, kernel_size=3, strides=2, padding='same'))
+model.add(Activation(LeakyReLU()))
+model.add(Conv2D(64, kernel_size=3, strides=2, padding='same'))
+model.add(Activation(LeakyReLU()))
+model.add(MaxPooling2D())
+model.add(Conv2D(128, kernel_size=3, strides=2, padding='same'))
+model.add(Activation(LeakyReLU()))
+model.add(MaxPooling2D())
+model.add(Conv2D(256, kernel_size=3, strides=2, padding='same'))
+model.add(Activation(LeakyReLU()))
+#model.add(MaxPooling2D())
+
+model.add(Flatten())
+model.add(Dense(256, activation="relu"))
+model.add(Dense(1, activation='linear'))
+
+model.compile(loss="mse", optimizer="adam", metrics=["mae"])
+
+model.summary()
+callbacks = list()
+callbacks.append(keras.callbacks.TensorBoard(log_dir="./", histogram_freq=0, write_graph=True))
+callbacks.append(
+    keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=1, min_lr=1e-10))
+callbacks.append(keras.callbacks.EarlyStopping(monitor='val_loss', patience=20))
+callbacks.append(keras.callbacks.TerminateOnNaN())
+
+model.fit_generator(
+    generator=train,
+    epochs=500,
+    verbose=2,
+    validation_data=val,
+    callbacks=callbacks,
+    use_multiprocessing=False,
+    workers=1,
+    max_queue_size=100,
+)
--- redshift_loader.py
+++ redshift_loader.py
@@ -3,28 +3,32 @@ import numpy as np
 from keras.utils import Sequence
 import matplotlib.pyplot as plt
 from astropy.io import fits
+from sklearn.utils import shuffle
 import h5py
 
 
 class DataLoader(Sequence):
-    def __init__(self, dataset_name, batch_size, img_res=(101, 101), norm=False):
-        self.dataset_name = dataset_name
+    def __init__(self, batch_size, img_res=(101, 101), norm=False, data=None):
         self.batch_size = batch_size
         self.img_res = img_res
-        self.data = np.loadtxt("network/networkFrame.csv", delimiter=',', dtype=str)
+        if data is None:
+            self.data = np.loadtxt("network/networkFrame.csv", delimiter=',', dtype=str)
+        else:
+            self.data = data
         self.h5 = h5py.File("network/0-4000.hdf5", mode='r')
+        self.h52 = h5py.File("network/4000-7137.hdf5", mode='r')
         self.norm = norm
 
     def __getitem__(self, item):
         batch_images = self.data[(item*self.batch_size):((item+1)*self.batch_size)]
-
-        return NotImplementedError
+        return self.load_batch(batch_images)
 
     def __len__(self):
-        return NotImplementedError
+        return int(len(self.data)/self.batch_size)
 
     def on_epoch_end(self):
-        return NotImplementedError
+        self.data = shuffle(self.data)
+
 
     def load_image(self, path):
         sim_img = []
@@ -42,78 +46,32 @@ class DataLoader(Sequence):
         return sim_img
 
     def load_redshift(self, object_id):
+        object_id = object_id - 1
         if object_id < 4000:
-            redshift = self.h5[object_id]["z_source"]
+            data = self.h5
+            redshift = data["z_source"][object_id]
+        else:
+            data = self.h52
+            redshift = data["z_source"][int(object_id - 4000)]
+        return redshift
 
-
-
-    def load_data(self, batch_size=1, is_testing=False):
-        batch_images = np.random.choice(self.data.shape[0], size=batch_size)
-
+    def load_batch(self, paths):
+        batch = paths
         imgs_A = []
-        imgs_B = []
-        for img_path in batch_images:
-            img_A, img_B = self.imread(self.data[img_path])
+        redshifts = []
+        for img in batch:
+            # Need to load in 5 channels here for the data
+            img_A = self.load_image(img)
+            redshift = self.load_redshift(int(img[0]))
 
-            # If training => do random flip
-            if not is_testing and np.random.random() < 0.5:
+            if np.random.random() > 0.5:
                 img_A = np.fliplr(img_A)
-                img_B = np.fliplr(img_B)
-            if not is_testing and np.random.random() > 0.5:
+            if np.random.random() < 0.5:
                 img_A = np.flipud(img_A)
-                img_B = np.flipud(img_B)
 
             imgs_A.append(img_A)
-            imgs_B.append(img_B)
-
+            redshifts.append(redshift)
         imgs_A = np.asarray(imgs_A)
-        imgs_B = np.asarray(imgs_B)
-        return imgs_A, imgs_B
-
-    def load_batch(self, batch_size=1, is_testing=False):
-
-        self.n_batches = int(len(self.data) / batch_size)
-
-        for i in range(self.n_batches - 1):
-            batch = self.data[i * batch_size:(i + 1) * batch_size]
-            imgs_A, imgs_B = [], []
-            for img in batch:
-                # Need to load in 5 channels here for the data
-                img_A, img_B = self.imread(img)
-
-                if not is_testing and np.random.random() > 0.5:
-                    img_A = np.fliplr(img_A)
-                    img_B = np.fliplr(img_B)
-                if not is_testing and np.random.random() < 0.5:
-                    img_A = np.flipud(img_A)
-                    img_B = np.flipud(img_B)
-
-                imgs_A.append(img_A)
-                imgs_B.append(img_B)
-            imgs_A = np.asarray(imgs_A)
-            imgs_B = np.asarray(imgs_B)
-            yield imgs_A, imgs_B
-
-    def imread(self, path):
-        sim_img = []
-        source_img = []
-        for element in path[1:]:
-            if "sci" in element:
-                if "sim" in element:
-                    img = fits.getdata(element, ext=0)
-                    center = (int(img.shape[0] / 2), int(img.shape[1] / 2))
-                    img = img[center[0] - 32:center[0] + 32, center[1] - 32:center[1] + 32]
-                    sim_img.append(img)
-                elif "source" in element:
-                    img = fits.getdata(element, ext=0)
-                    center = (int(img.shape[0] / 2), int(img.shape[1] / 2))
-                    img = img[center[0] - 32:center[0] + 32, center[1] - 32:center[1] + 32]
-                    source_img.append(img)
-        sim_img = np.asarray(sim_img).T
-        source_img = np.asarray(source_img).T
-        if self.norm:
-            sim_img = 2 * (sim_img - np.min(sim_img)) / (np.max(sim_img) - np.min(sim_img)) - 1.
-            source_img = 2 * (source_img - np.min(source_img)) / (np.max(source_img) - np.min(source_img)) - 1.
-
-        return sim_img, source_img
+        redshifts = np.asarray(redshifts)
+        return imgs_A, redshifts
 
